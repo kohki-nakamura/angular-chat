b6deadd30e44c2e4ae4eb4e085a373393b62d9b9
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-06-24 23:28:50 +0900

    fix: 72. タイムラインを表示する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+
+const COMMENTS: Comment[] = [
+  { name: "武井 賢治", message: 'お疲れさまです！'},
+  { name: "武井 賢治", message: 'この間の件ですが、どうなりましたか？'},
+  { name: "五十川 洋平", message: 'お疲れさまです！'}
+]
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat';
+  
+  comments = COMMENTS;
 }
