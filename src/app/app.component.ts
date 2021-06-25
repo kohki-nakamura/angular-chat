f6a8638edfc81bb8999bca2c3063dc0f2ec78d85
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-06-26 03:15:32 +0900

    fix: 74. チャットを投稿できるようにする

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,4 +22,11 @@ export class AppComponent {
   
   comments = COMMENTS;
   currentUser = CURRENT_USER;
+  comment = '';
+
+  addComment(comment: string): void {
+    if (comment) {
+      this.comments.push(new Comment(this.currentUser, comment));
+    }
+  }
 }
