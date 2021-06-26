e4081d486dc113a781c92c52e0c679610451a181
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-06-26 15:48:18 +0900

    fix: 83. チャットの編集と削除を実装する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,10 +20,8 @@ export class AppComponent {
   commentsRef: AngularFireList<Comment>;
   currentUser = CURRENT_USER;
   comment = '';
-  item$: Observable<any>;
 
   constructor(private db: AngularFireDatabase) {
-    this.item$ = db.object('/item').valueChanges();
     this.commentsRef = db.list('/comments');
     this.comments$ = this.commentsRef.snapshotChanges()
       .pipe(
@@ -41,5 +39,15 @@ export class AppComponent {
       this.commentsRef.push(new Comment({ user: this.currentUser, message: comment }));
       this.comment = '';
     }
+  }
+
+  updateComment(comment: Comment): void {
+    const { key, message } = comment;
+
+    this.commentsRef.update(key!, { message });
+  }
+
+  deleteComment(comment: Comment): void {
+    this.commentsRef.remove(comment.key);
   }
 }
