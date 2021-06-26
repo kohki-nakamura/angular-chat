e38491c6d436b9ffdaff734edb75da65c2729aa2
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-06-26 22:32:32 +0900

    fix: 93. ログイン・ログアウトを実装する

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'ac-login',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  login(form: NgForm): void {
+    const { email, password } = form.value;
+
+    this.authService.login(email, password)
+      .then(() => this.router.navigateByUrl('/'));
+  }
 }
