cb2cb3e885e399b97df41d89352d829657682937
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-06-27 17:02:06 +0900

    fix: 102. アプリケーションを公開する

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAnt_0pF3XwOjp56mCCbJNuIapHC14S0ow",
+    authDomain: "angular-chat-b7534.firebaseapp.com",
+    projectId: "angular-chat-b7534",
+    storageBucket: "angular-chat-b7534.appspot.com",
+    messagingSenderId: "1041371265618",
+    appId: "1:1041371265618:web:47dd74d2b0e30a6aa3bd8b"
+  }
 };
