f7fc8003f33d6b810889101b89894355a827e79a
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-06-27 15:09:48 +0900

    fix: 98. ログインに応じてトップページの表示を切り替える

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -18,6 +18,7 @@ export class ChatComponent implements OnInit {
   comments$: Observable<Comment[]>;
   commentsRef: AngularFireList<Comment>;
   currentUser: User;
+  currentUser$: Observable<User | null>;
   comment = '';
 
   constructor(
@@ -28,11 +29,15 @@ export class ChatComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.afAuth.authState.subscribe((user: firebase.User | null) => {
-      if (user) {
-        this.currentUser = new User(user);
-      }
-    });
+    this.currentUser$ = this.afAuth.authState.pipe(
+      map((user: firebase.User | null) => {
+        if (user) {
+          this.currentUser = new User(user);
+          return this.currentUser;
+        }
+        return null;
+      })
+    )
 
     this.comments$ = this.commentsRef.snapshotChanges()
       .pipe(
